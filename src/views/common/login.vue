980dd2ed56f5c118c4e28ccad05fa1b91b537320
ygsit <183213557@qq.com>
2021-07-24 18:31:44 +0800

    update

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">YMall</h2>
+          <p class="brand-info__intro">全品类综合购物平台</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
@@ -116,7 +116,7 @@
       width: 100%;
       height: 100%;
       content: "";
-      background-image: url(~@/assets/img/login_bg.jpg);
+      background-image: url(~@/assets/img/background.jpg);
       background-size: cover;
     }
     .site-content__wrapper {
